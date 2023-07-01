9ccb9d1cf976fadac3bdb1bbcd9c2aa4415927f6
harishkumarforever <harishking809@gmail.com>
2023-07-01 20:50:06 +0500

    Create react cdn links.js

---
+++ react cdn links.js
@@ -0,0 +1,3 @@
+ <script src="https://unpkg.com/@babel/standalone/babel.js"></script>
+ <script src="https://unpkg.com/react/umd/react.development.js"></script>
+ <script src="https://unpkg.com/react-dom/umd/react-dom.development.js"></script>
